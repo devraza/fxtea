e5ed1a444ce5023187c9ba143615ea65d4e1c40b
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-01-27 14:19:12 +0000

    feat: poisson CD (`>` and `<`)

--- poisson/main.py
+++ poisson/main.py
@@ -1,11 +1,21 @@
 import math
 
-a = list(map(float, input().split(' ')))
-rate = a[0]
+a, b = input().split(' ')
+
+rate = float(a)
+i = b[0]
+b = int(b[1:])
 
 sums = 0
 
-for i in range(1,len(a)):
-    sums += round(math.e**(-1*rate) * (rate**a[i]/math.factorial(int(a[i]))), 4)
+if i == ">":
+    for j in range(b+1):
+        sums += math.e**(-1*rate) * (rate**j/math.factorial(j))
+    print(round(1-sums, 4))
+elif i == "<":
+    for j in range(b):
+        sums += math.e**(-1*rate) * (rate**j/math.factorial(j))
+    print(round(sums, 4))
+elif i == "=":
+    print(round(math.e**(-1*rate) * (rate**b/math.factorial(b)), 4))
 
-print(sums)
