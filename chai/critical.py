06109515ae9a3c9b86d920891b8b952ee772bcda
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-02-24 21:05:49 +0000

    refactor: use `math.gamma()` instead of implementing it ourselves

--- chai/critical.py
+++ chai/critical.py
@@ -1,22 +1,5 @@
 import math
 
-def gamma(x):
-    if x == int(x):
-        return math.factorial(int(x) - 1)
-    p = [
-        676.5203681218851, -1259.1392167224028, 771.32342877765313,
-        -176.61502916214059, 12.507343278686905, -0.13857109526572012,
-        9.9843695780195716e-6, 1.5056327351493116e-7
-    ]
-    g = 7
-    if x < 0.5:
-        return math.pi / (math.sin(math.pi * x) * gamma_function(1 - x))
-    x -= 1
-    a = 0.99999999999980993
-    for i, val in enumerate(p):
-        a += val / (x + i + 1)
-    return math.sqrt(2 * math.pi) * (x + g + 0.5) ** (x + 0.5) * math.exp(-(x + g + 0.5)) * a
-
 def li_gamma(s, x):
     def integrand(t):
         return t**(s - 1) * math.exp(-t)
@@ -32,7 +15,7 @@ def chi(df, alpha, tol=1e-6):
     low, high = 0, 100
     while high-low>tol:
         mid = (low+high)/2
-        if li_gamma(df/2, mid/2) / gamma(df/2) < 1-alpha:
+        if li_gamma(df/2, mid/2) / math.gamma(df/2) < 1-alpha:
             low = mid
         else:
             high = mid
