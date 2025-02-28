95c0c0eda3db1f0599d11f0f6bcba089bc117b08
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-02-28 09:23:27 +0000

    refactor: rename `CD` -> `PoissonCD`

--- fx/poisson.go
+++ fx/poisson.go
@@ -23,7 +23,7 @@ func PoissonPD(lambda float64, x uint64) float64 {
   return math.Pow(math.E, -lambda) * float64(pow(uint64(lambda), x)/factorial(x))
 }
 
-func CD(lambda float64, x uint64) float64 {
+func PoissonCD(lambda float64, x uint64) float64 {
   cumulative := 0.
   for i := range x {
     cumulative += PoissonPD(lambda, i)
