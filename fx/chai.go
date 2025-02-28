9e22288d40df7a8edadb0d9ab7f5678b1a5bf773
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-02-28 20:34:08 +0000

    feat: port critical values for Chi-squared to Golang

--- fx/chai.go
+++ fx/chai.go
@@ -1,7 +1,18 @@
 package fx
 
-import "math"
+import (
+  "math"
+  "gonum.org/v1/gonum/stat/distuv"
+)
 
-func Sum(o float64, e float64) float64 {
+func ChiSum(o float64, e float64) float64 {
   return math.Pow((o-e), 2)/e
+}
+
+func ChiCritical(df float64, alpha float64) float64 {
+  chi := &distuv.ChiSquared {
+	  K: df,
+    Src: nil,
+  }
+  return chi.Quantile(1-alpha)
 }
